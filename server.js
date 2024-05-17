26257386d982b9b62b7dec1776c3ab72611f4104
fchzo08 <89438930+fchzo08@users.noreply.github.com>
2024-05-17 12:26:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const bcrypt = require('bcryptjs');
 const axios = require('axios');
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use(bodyParser.json());
 
 async function verifyToken(req, res, next) {
